487b1a3acf1b60b76fcd13e3de42f53ba959078e
Mark Vogt <markevogt@outlook.com>
2024-03-24 20:04:59 -0500

    CLONED generateResponse1() to generateResponse2()...

--- function_app.py
+++ function_app.py
@@ -3,6 +3,28 @@ import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
+@app.route(route="generateResponse1")
+def generateResponse1(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP-triggered function generateResponse1() processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP-triggered function generateResponse1() executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP-triggered function generateResponse1() executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+    
+
 @app.route(route="generateResponse2")
 def generateResponse2(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP-triggered function generateResponse2() processed a request.')
@@ -22,4 +44,4 @@ def generateResponse2(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(
              "This HTTP-triggered function generateResponse2() executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
-        )
+        )    
